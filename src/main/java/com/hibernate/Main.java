ea49d0104d3667a5670989f089667039925924b4
Technical Savage <168007796+Nalin2k@users.noreply.github.com>
2025-02-10 16:21:01 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/Main.java
@@ -0,0 +1,29 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import com.hibernate.model.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee vikas = new Employee("Vikas Kumar", "Male", 44000);
+//		Configuration cfg = new Configuration().configure(); 
+//		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory sf = meta.buildSessionFactory();
+		Session session = sf.openSession();
+		session.persist(vikas);        
+		Transaction tx = session.beginTransaction();
+		tx.commit();
+	}
+
+}
