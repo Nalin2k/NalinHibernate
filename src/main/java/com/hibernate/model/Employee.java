84656c470021b2668b56c0d67bac9817233174c2
Technical Savage <168007796+Nalin2k@users.noreply.github.com>
2025-02-12 00:27:36 +0530

    update

--- src/main/java/com/hibernate/model/Employee.java
+++ src/main/java/com/hibernate/model/Employee.java
@@ -2,14 +2,16 @@ package com.hibernate.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "facebook_emp")
 public class Employee {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
-	
 	@Column(name = "emp_name")
 	String name; 
 	String gender;
